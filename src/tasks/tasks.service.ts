c1707ea7838d700caed66f3222fb81b9d8caef03
Ayush <ayush.mohjay@gmail.com>
2025-11-05 17:35:35 +0530

    first commit

---
+++ src/tasks/tasks.service.ts
@@ -0,0 +1,45 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { Task } from './task.model';
+import {v4 as uuid} from 'uuid';
+import { CreateTaskDto } from './dto/create-task.dto';
+
+@Injectable()
+export class TasksService {
+    private tasks:Task[] = [];
+
+    getAllTasks(): Task[]{
+        return this.tasks;
+    }
+
+    getTaskById(id:string):Task {
+        const task =  this.tasks.find((task)=>task.id === id);
+
+        if(!task){
+            throw new NotFoundException(`Task with Id "${id}" not found`)
+        }
+
+        return task;
+    }
+
+    createTask(createTaskDto: CreateTaskDto):Task{
+        const {title,description} = createTaskDto;
+        const task:Task = {
+            id:uuid(),
+            title,
+            description,
+            status:'OPEN'
+        };
+        this.tasks.push(task);
+        return task;
+    }
+
+    deleteTask(id:string):void{
+        this.tasks = this.tasks.filter((task)=> task.id !== id);
+    }
+
+    updateTaskStatus(id:string,status:Task['status']):Task{
+        const task = this.getTaskById(id);
+        task.status = status;
+        return task;
+    }
+}
